a89da869d7919d0e85e916a4b784f0c4152f6294
idanZ1 <idanzaid07@gmail.com>
2023-11-16 16:21:32 +0200

    תיקון של שיעורי הבית מהתאריך 11/11/2023

--- Q1.py
+++ Q1.py
@@ -1,7 +1,7 @@
 #שאלה מספר 1
 str = input("Enter your String")
 num = int(input("Enter your Number"))
-if len(str) > num:
-    print(str[num])
+if (len(str) > num and num>0):
+    print(str[num-1])
 else:
     print("String isn't long enough")
